a87a8059b9accc051cc2345f6396d2083a76f57c
NhatMinh2208 <minh33haha@gmail.com>
2022-09-30 16:56:02 +0700

    Add adder lib

---
+++ src/Adder/add.cpp
@@ -0,0 +1,4 @@
+#include"add.h"
+float add(float a, float b){
+    return a+b;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-
+#include"Adder/add.h"
+float add(float a, float b);
 int main(){
-    std::cout << "Hello World 1";
+    std::cout << "Hello World 1" << add(3.0f, 4.0f);
 }

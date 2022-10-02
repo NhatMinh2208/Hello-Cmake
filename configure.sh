330113cda34ebe46f87970976ed6e3737cc3b17b
NhatMinh2208 <minh33haha@gmail.com>
2022-10-02 17:38:46 +0700

    Install, make libs option, find_lib

---
+++ configure.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+cmake -DGLFW_BUILD_DOCS=OFF -DUSE_ADDER=ON -S . -B ./build3
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
-#include"Adder/add.h"
+#include"GLFW/glfw3.h"
+#include"configure.h"
+#ifdef USE_ADDER
+    #include"Adder/add.h"
+#endif
 float add(float a, float b);
 int main(){
-    std::cout << "Hello World 1" << add(3.0f, 4.0f);
+    #ifdef USE_ADDER
+        std::cout << "Hello World 1" << add(3.0f, 4.0f);
+    #else   
+        std::cout << "Turn ON for adding" ;
+    #endif
+    
+    GLFWwindow* window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // Exit program
+    exit( EXIT_SUCCESS );
 }
